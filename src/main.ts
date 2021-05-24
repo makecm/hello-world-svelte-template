fa109a9e84183d8dc60dc179db9fa16235185972
samueljim <thesamueljim@gmail.com>
2021-05-24 13:56:55 +1000

    added auto build and default input values

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import App from './App.svelte';
 
+// You can define input default values in this file. The templateProps will be merged with the json 
 const app = new App({
   target: document.body,
   props: {
-    templateProps: window.templateProps || {
+    templateProps: { 
+    ...{
       headline: 'Hello World',
       showLogos: true,
       backgroundImage: ''
-    }
+    },
+    ...window.templateProps
   }
 });
 
